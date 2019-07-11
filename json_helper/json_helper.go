b36c81bea41adeb586a877ffb3df5a8fe44e93d8
resputin <resputin@gmail.com>
2019-07-11 17:03:03 +0800

    add json

---
+++ json_helper/json_helper.go
@@ -0,0 +1,16 @@
+package jsonHelper
+
+
+import (
+	"encoding/json"
+	"bytes"
+)
+
+
+func JSONMarshal(t interface{}) ([]byte, error) {
+	buffer := &bytes.Buffer{}
+	encoder := json.NewEncoder(buffer)
+	encoder.SetEscapeHTML(false)
+	err := encoder.Encode(t)
+	return buffer.Bytes(), err
+}
