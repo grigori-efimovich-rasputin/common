83947b9b29c68dca557392fff37c5c8380cf2ab7
resputin <resputin@gmail.com>
2019-07-17 17:09:35 +0800

    up

--- sync_helper/sync_helper.go
+++ sync_helper/sync_helper.go
@@ -2,26 +2,34 @@ package syncHelper
 
 import (
 	"sync/atomic"
+	"runtime"
+	"sync"
 )
 
 
 type AtomicLock struct {
+	_    sync.Mutex // for copy protection compiler warning
 	flag uint32
 }
-
 
 func NewAtomicLock() *AtomicLock{
 	return &AtomicLock{flag: 0}
 }
 
-func (s *AtomicLock)TryLock() bool {
+func (s *AtomicLock) Lock() {
+	for !atomic.CompareAndSwapUint32(&s.flag, 0, 1) {
+		runtime.Gosched()
+	}
+}
+
+func (s *AtomicLock) TryLock() bool {
 	return atomic.CompareAndSwapUint32(&s.flag, 0, 1)
 }
 
-func (s *AtomicLock)TryUnlock() bool {
+func (s *AtomicLock) TryUnlock() bool {
 	return atomic.CompareAndSwapUint32(&s.flag, 1, 0)
 }
 
-func (s *AtomicLock)Unlock() {
+func (s *AtomicLock) Unlock() {
 	atomic.StoreUint32(&s.flag, 0)
 }
